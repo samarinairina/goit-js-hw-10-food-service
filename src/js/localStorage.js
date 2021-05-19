20cb9388bea321a58a962645043f61f9655cbfda
irina <samarinairina2017@gmail.com>
2021-05-19 19:57:35 +0300

    Убрала двойную запись в Local

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -5,32 +5,30 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
-localStorage.setItem('theme', JSON.stringify(Theme));
+ localStorage.setItem('theme', JSON.stringify(Theme));
 
 const savedTheme = localStorage.getItem('theme');
 
 const parsedTheme = JSON.parse(savedTheme);
 
  
-// let currentThemeParse = JSON.parse(localStorage.getItem('currentTheme'));
 
-// refs.body.classList.add(currentThemeParse);
-
-// if (refs.body.classList.contains(parsedTheme.DARK)) {
-//   refs.themeSwitch.checked = true;
-// }
+refs.body.classList.add(Theme.DARK)
+if (refs.body.classList.contains(parsedTheme.DARK)) {
+  refs.themeSwitch.checked = true;
+}
 
 function changeTheme() {
-    if (refs.body.classList.contains(parsedTheme.DARK)) {
-        refs.body.classList.remove(parsedTheme.DARK);
-        refs.body.classList.add(parsedTheme.LIGHT);
-        localStorage.removeItem('Theme', JSON.stringify(parsedTheme.DARK));
-        localStorage.setItem('Theme', JSON.stringify(parsedTheme.LIGHT));
+    if (refs.body.classList.contains(Theme.DARK)) {
+        refs.body.classList.remove(Theme.DARK);
+        refs.body.classList.add(Theme.LIGHT);
+        localStorage.removeItem('theme', JSON.stringify(parsedTheme.DARK));
+        localStorage.setItem('theme', JSON.stringify(parsedTheme.LIGHT));
     } else {
-        refs.body.classList.remove(parsedTheme.LIGHT);
-        refs.body.classList.add(parsedTheme.DARK);
-        localStorage.removeItem('Theme', JSON.stringify(parsedTheme.LIGHT));
-        localStorage.setItem('Theme', JSON.stringify(parsedTheme.DARK));
+        refs.body.classList.remove(Theme.LIGHT);
+        refs.body.classList.add(Theme.DARK);
+        localStorage.removeItem('theme', JSON.stringify(parsedTheme.LIGHT));
+        localStorage.setItem('theme', JSON.stringify(parsedTheme.DARK));
     };
 }
 
